c4e8810de0ba070ad89a07115fae6a83c9220d60
Hye Woong Jeon <ajeon66@gmail.com>
2024-01-03 06:07:19 -0600

    20240103 - implemented first test for parser

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,8 @@
 package ast
 
+import "monkey/token"
+
+// Tells you the literal information in the node.
 type Node interface {
 	TokenLiteral() string
 }
@@ -12,4 +15,38 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+// Root AST node
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // "let"
+	Name  *Identifier // identifier on the LHS
+	Value *Expression // stuff on the RHS
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
