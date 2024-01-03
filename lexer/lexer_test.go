54a6a397ba5857fe34d149e244686e2d853166fd
Hye Woong Jeon <ajeon66@gmail.com>
2024-01-02 20:43:50 -0600

    20240102 - finished chapter 1

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -47,6 +47,18 @@ func TestNextToken2(t *testing.T) {
 		}
 
 		let result = add(five, ten);
+
+		!-/*5;
+		5 < 10 > 5;
+
+		if (5 < 10) {
+			return true;
+		} else {
+			return false;
+		}
+
+		10 == 10;
+		10 != 9;
 	`
 	tests := []struct {
 		expectedType    token.TokenType
@@ -86,6 +98,18 @@ func TestNextToken2(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 	}
 
