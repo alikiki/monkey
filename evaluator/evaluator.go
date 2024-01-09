d59731c1e6df38d7b1e9a90474e6d28407fd423d
Hye Woong Jeon <ajeon66@gmail.com>
2024-01-08 20:19:59 -0600

    20240108 - exiting at return

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -58,7 +58,7 @@ func evalBlockStatement(block *ast.BlockStatement) object.Object {
 	var result object.Object
 
 	for _, statement := range block.Statements {
-		result := Eval(statement)
+		result = Eval(statement)
 
 		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
 			return result
