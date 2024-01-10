5424ea13cffbb219c6bfad7d637768a0e8ac3063
Hye Woong Jeon <ajeon66@gmail.com>
2024-01-10 07:23:22 -0600

    added functions and function calls

--- object/environment.go
+++ object/environment.go
@@ -1,20 +1,48 @@
 package object
 
+import "fmt"
+
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+
+	return env
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
+
 	return obj, ok
 }
 
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
+}
+
+func (e *Environment) SeeStore() {
+	for k, v := range e.store {
+		fmt.Println(k, ": ", v)
+	}
+	if e.outer != nil {
+		fmt.Println("---")
+		e.outer.SeeStore()
+	}
+}
+
+func (e *Environment) OuterExists() bool {
+	return e.outer != nil
 }
--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
 
 type ObjectType string
 
@@ -21,6 +26,12 @@ type ReturnValue struct {
 	Value Object
 }
 
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
 type Null struct{}
 
 type Error struct {
@@ -33,6 +44,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
@@ -58,4 +70,23 @@ func (rv *ReturnValue) Inspect() string {
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string {
 	return "ERROR: " + e.Message
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }
